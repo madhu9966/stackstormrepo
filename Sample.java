4668ff1a262f6e3012accf08a8a40b348ac6cfb8
madhu9999 <priyaneelam4u@gmail.com>
2019-04-01 18:11:57 +0530

    file commit

---
+++ Sample.java
@@ -0,0 +1,16 @@
+
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Sample {
+	public static void main(String[] args) {
+		List<String> l=new ArrayList<String>();
+		l.add("Madhu");
+		l.add("priya");
+		l.add("Sunny");
+		System.out.println(l);
+		
+	}
+
+}
