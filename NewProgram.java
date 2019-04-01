1fd7ac2ba7343a328d85b33799fd52391eeb23a5
madhu9999 <priyaneelam4u@gmail.com>
2019-04-01 17:53:03 +0530

    first comment

---
+++ NewProgram.java
@@ -0,0 +1,16 @@
+package com.exam;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Sample {
+	public static void main(String[] args) {
+		List<String> l=new ArrayList<String>();
+		l.add("Madhu");
+		l.add("priya");
+		l.add("Sunny");
+		System.out.println(l);
+		
+	}
+
+}
